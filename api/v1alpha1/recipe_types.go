dd8fe2e7887307998111878a1a0166a34d2bc44b
hatfieldbrian <bhatfiel@redhat.com>
2022-12-01 15:52:33 -0800

    Replace recipe_types.go with Fusion version
    Signed-off-by: hatfieldbrian <bhatfiel@redhat.com>

--- api/v1alpha1/recipe_types.go
+++ api/v1alpha1/recipe_types.go
@@ -1,18 +1,5 @@
-/*
-Copyright 2022 IBM Corp.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// SPDX-FileCopyrightText: IBM Corp.
+// SPDX-License-Identifier: Apache2.0
 
 package v1alpha1
 
@@ -28,8 +15,131 @@ type RecipeSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of Recipe. Edit recipe_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Type of application the recipe is designed for. (AppType is not used yet. For now, we will
+	// match the name of the app CR)
+	AppType string `json:"appType"`
+	// List of one or multiple groups
+	Groups []*Group `json:"groups"`
+	// List of one or multiple hooks
+	Hooks []*Hook `json:"hooks,omitempty"`
+	// Workflow is the sequence of actions to take
+	Workflows []*Workflow `json:"workflows"`
+}
+
+// Groups defined in the recipe refine / narrow-down the scope of its parent groups defined in the
+// Application CR. Recipe groups are always be associated to a parent group in Application CR -
+// explicitly or implicitly. Recipe groups can be used in the context of backup and/or restore workflows
+type Group struct {
+	// Name of the group
+	Name string `json:"name"`
+	// Name of the parent group defined in the associated Application CR. Optional - If unspecified,
+	// parent group is represented by the implicit default group of Application CR (implies the
+	// Application CR does not specify groups explicitly).
+	Parent string `json:"parent,omitempty"`
+	// Used for groups solely used in restore workflows to refer to another group that is used in
+	// backup workflows.
+	BackupRef string `json:"backupRef,omitempty"`
+	// Determines the type of group - volume data only, resources only
+	// +kubebuilder:validation:Enum=volume;resource
+	Type string `json:"type"`
+	// List of resource types to include. If unspecified, all resource types are included.
+	IncludedResourceTypes []string `json:"includedResourceTypes,omitempty"`
+	// List of resource types to exclude
+	ExcludedResourceTypes []string `json:"excludedResourceTypes,omitempty"`
+	// Select items based on label
+	LabelSelector string `json:"labelSelector,omitempty"`
+	// If specified, resource's object name needs to match this expression. Valid for volume groups only.
+	NameSelector string `json:"nameSelector,omitempty"`
+	// Determines the resource type which the fields labelSelector and nameSelector apply to for selecting PVCs. Default selection is pvc. Valid for volume groups only.
+	// +kubebuilder:validation:Enum=pvc;pod;deployment;statefulset
+	// +kubebuilder:validation:Optional
+	SelectResource string `json:"selectResource,omitempty"`
+	// Whether to include any cluster-scoped resources. If nil or true, cluster-scoped resources are
+	// included if they are associated with the included namespace-scoped resources
+	IncludeClusterResources *bool `json:"includeClusterResources,omitempty"`
+	// Selects namespaces by label
+	IncludedNamespacesByLabel string `json:"includedNamespacesByLabel,omitempty"`
+	// List of namespaces to include.
+	IncludedNamespaces []string `json:"includedNamespaces,omitempty"`
+	// List of namespace to exclude
+	ExcludedNamespaces []string `json:"excludedNamespaces,omitempty"`
+	// Defaults to true, if set to false, a failure is not necessarily handled as fatal
+	Essential *bool `json:"essential,omitempty"`
+}
+
+// Workflow is the sequence of actions to take
+type Workflow struct {
+	// Name of recipe. Names "backup" and "restore" are reserved and implicitly used by default for
+	// backup or restore respectively
+	Name string `json:"name"`
+	// List of the names of groups or hooks, in the order in which they should be executed
+	// Format: <group|hook>: <group or hook name>[/<hook op>]
+	Sequence []map[string]string `json:"sequence"`
+	// Implies behaviour in case of failure: any-error (default), essential-error, full-error
+	// +kubebuilder:validation:Enum=any-error;essential-error;full-error
+	// +kubebuilder:default=any-error
+	FailOn string `json:"failOn,omitempty"`
+}
+
+// Hooks are actions to take during recipe processing
+type Hook struct {
+	// Hook name, unique within the Recipe CR
+	Name string `json:"name"`
+	// Namespace
+	Namespace string `json:"namespace"`
+	// Hook type
+	// +kubebuilder:validation:Enum=exec;scale;check
+	Type string `json:"type"`
+	// Resource type to that a hook applies to
+	// +kubebuilder:validation:Enum=pod;deployment;statefulset
+	SelectResource string `json:"selectResource,omitempty"`
+	// If specified, resource object needs to match this label selector
+	LabelSelector string `json:"labelSelector,omitempty"`
+	// If specified, resource's object name needs to match this expression
+	NameSelector string `json:"nameSelector,omitempty"`
+	// Boolean flag that indicates whether to execute command on a single pod or on all pods that
+	// match the selector
+	SinglePodOnly bool `json:"singlePodOnly,omitempty"`
+	// Default behavior in case of failing operations (custom or built-in ops). Defaults to Fail.
+	// +kubebuilder:validation:Enum=fail;continue
+	// +kubebuilder:default=fail
+	OnError string `json:"onError,omitempty"`
+	// Default timeout in seconds applied to custom and built-in operations. If not specified, equals to 30s.
+	Timeout int `json:"timeout,omitempty"`
+	// Set of operations that the hook can be invoked for
+	Ops []*Operation `json:"ops,omitempty"`
+	// Set of checks that the hook can apply
+	Chks []*Check `json:"chks,omitempty"`
+	// Defaults to true, if set to false, a failure is not necessarily handled as fatal
+	Essential *bool `json:"essential,omitempty"`
+}
+
+// Operation to be invoked by the hook
+type Operation struct {
+	// Name of the operation. Needs to be unique within the hook
+	Name string `json:"name"`
+	// The container where the command should be executed
+	Container string `json:"container,omitempty"`
+	// The command to execute
+	Command string `json:"command"`
+	// How to handle command returning with non-zero exit code. Defaults to Fail.
+	OnError string `json:"onError,omitempty"`
+	// How long to wait for the command to execute, in seconds
+	Timeout int `json:"timeout,omitempty"`
+	// Name of another operation that reverts the effect of this operation (e.g. quiesce vs. unquiesce)
+	InverseOp string `json:"inverseOp,omitempty"`
+}
+
+// Operation to be invoked by the hook
+type Check struct {
+	// Name of the check. Needs to be unique within the hook
+	Name string `json:"name"`
+	// The condition to check for
+	Condition string `json:"condition,omitempty"`
+	// How to handle when check does not become true. Defaults to Fail.
+	OnError string `json:"onError,omitempty"`
+	// How long to wait for the check to execute, in seconds
+	Timeout int `json:"timeout,omitempty"`
 }
 
 // RecipeStatus defines the observed state of Recipe
@@ -39,6 +149,7 @@ type RecipeStatus struct {
 }
 
 //+kubebuilder:object:root=true
+//+kubebuilder:resource:categories="isf",shortName="frcpe"
 //+kubebuilder:subresource:status
 
 // Recipe is the Schema for the recipes API
