50012f6ad481b215f4527a776d2bd33fb6fcdae5
Travis Janssen <travis.janssen@ibm.com>
2023-04-12 14:38:59 -0700

    timeout fields: change from int to metav1.Duration - allow user to input timeout durations with a time value and unit (e.g. "30s" or "5m"), rather than integer seconds - issue #8
    Signed-off-by: Travis Janssen <travis.janssen@ibm.com>

--- api/v1alpha1/recipe_types.go
+++ api/v1alpha1/recipe_types.go
@@ -102,8 +102,10 @@ type Hook struct {
 	// +kubebuilder:validation:Enum=fail;continue
 	// +kubebuilder:default=fail
 	OnError string `json:"onError,omitempty"`
-	// Default timeout in seconds applied to custom and built-in operations. If not specified, equals to 30s.
-	Timeout int `json:"timeout,omitempty"`
+	// Default timeout applied to custom and built-in operations. If not specified, equals to 30s.
+	//+kubebuilder:validation:Format=duration
+	//+optional
+	Timeout *metav1.Duration `json:"timeout,omitempty"`
 	// Set of operations that the hook can be invoked for
 	//+listType=map
 	//+listMapKey=name
@@ -127,8 +129,10 @@ type Operation struct {
 	Command []string `json:"command"`
 	// How to handle command returning with non-zero exit code. Defaults to Fail.
 	OnError string `json:"onError,omitempty"`
-	// How long to wait for the command to execute, in seconds
-	Timeout int `json:"timeout,omitempty"`
+	// How long to wait for the command to execute
+	//+kubebuilder:validation:Format=duration
+	//+optional
+	Timeout *metav1.Duration `json:"timeout,omitempty"`
 	// Name of another operation that reverts the effect of this operation (e.g. quiesce vs. unquiesce)
 	InverseOp string `json:"inverseOp,omitempty"`
 }
@@ -141,8 +145,10 @@ type Check struct {
 	Condition string `json:"condition,omitempty"`
 	// How to handle when check does not become true. Defaults to Fail.
 	OnError string `json:"onError,omitempty"`
-	// How long to wait for the check to execute, in seconds
-	Timeout int `json:"timeout,omitempty"`
+	// How long to wait for the check to execute
+	//+kubebuilder:validation:Format=duration
+	//+optional
+	Timeout *metav1.Duration `json:"timeout,omitempty"`
 }
 
 // RecipeStatus defines the observed state of Recipe
